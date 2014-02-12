8291ae85313f530316c971497117c9b16d0fff32
Zifei Shan <shanzifei@pku.edu.cn>
2014-02-11 22:52:53 -0800

    Updated 1gram scripts

--- udf/clean_filtered_labels.sh
+++ udf/clean_filtered_labels.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/env bash
-psql -c "TRUNCATE filtered_labels CASCADE;" ddocr
+# psql -c "TRUNCATE filtered_labels CASCADE;" ddocr
 psql -c "DROP TABLE IF EXISTS filtered_labels CASCADE;" ddocr
+psql -c """create table filtered_labels(id bigserial primary key, 
+  docid TEXT, 
+  wordid INT, 
+  label_t BOOLEAN, 
+  label_c BOOLEAN);""" ddocr

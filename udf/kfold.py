767cd4f7e5dbde66dd4c65d6016bcc79e4946dcf
Zifei Shan <shanzifei@pku.edu.cn>
2014-02-15 21:53:11 -0800

    Updated kfold with documentation

--- udf/kfold.py
+++ udf/kfold.py
@@ -1,11 +1,25 @@
 #! /usr/bin/env python
 
-# Call: /udf/kfold.py "${KFOLD_NUM}" "${KFOLD_ITER}"filtered_labels label_t label_c
+# Usage: kfold.py KFOLD_NUM KFOLD_ITER temporal_table_to_fold query_col_1 [query_col_2 ...]
+# KFOLD_NUM (K): number of folds to do evaluation
+# KFOLD_ITER (i): current number of iterations. range: 1..KFOLD_NUM
+
+# Functionality: 
+  # In temporal_table_to_fold,
+  # For rows in fraction [ (i-1)/K, (i)/K ), e.g. 0%--25% for K=4, i=1,
+  # Columns specified in query_col_1 [, query_col_2 ... ] 
+  # will be DELETED. 
+
+# Should be used as AFTER function of an extractor that generates temporal_table_to_fold.
+
+# Works better with a run script with iterations, changing input KFOLD_ITER.
+
+# e.g.: after: ${APP_HOME}"/udf/kfold.py "${KFOLD_NUM}" "${KFOLD_ITER}" filtered_labels label_t label_c"
 
 import os, sys
 
 # DEBUG
-print sys.argv
+print '-------- KFOLD INPUT ARGS: --------\n', sys.argv
 
 if len(sys.argv) >= 5:
   KFOLD_NUM = sys.argv[1]
@@ -34,4 +48,4 @@ fullquery = 'psql -c """'+'\n'.join(sql_queries)+'""" '+os.environ['DBNAME']
 os.system(fullquery)
 
 # DEBUG
-print fullquery
+print "-------- KFOLD QUERY --------\n", fullquery

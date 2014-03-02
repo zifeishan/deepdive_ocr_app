0bf42a7bebe3fb054d8c558684ac8a6800f1de4f
Zifei Shan <shanzifei@pku.edu.cn>
2014-03-01 19:08:14 -0800

    Updated scripts for new test data

--- udf/ext_cand_label.py
+++ udf/ext_cand_label.py
@@ -4,15 +4,10 @@ import fileinput
 import json
 from collections import defaultdict
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-
 # For each input tuple
 for row in fileinput.input():
   obj = json.loads(row)
   cid = obj["candidate.id"]
-  for i in range(0, len(fnames)):
-    if fvals[i] == False:
-      continue
-    print json.dumps({
-      "id": cid
-    })
+  print json.dumps({
+    "id": cid
+  })
